b3c6b1b9a105b4a0f87684c3311e5ebde6626610
RuffleSteels <rafferty.smith@hotmail.com>
2026-04-20 19:04:45 +0100

    more

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -2,6 +2,13 @@
 
 import { SessionProvider } from "next-auth/react";
 
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/coursework" : "";
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+      <SessionProvider basePath={`${basePath}/api/auth`}>
+        {children}
+      </SessionProvider>
+  );
 }
